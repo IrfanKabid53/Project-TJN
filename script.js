c368824e88f6b6e144571f919f67e2c36f74bfdc
IrfanKabid53 <78926873+IrfanKabid53@users.noreply.github.com>
2026-02-04 12:54:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,472 @@
+
+function check(){let p=document.getElementById("pass").value.toLowerCase();if(p=="madam"){window.location="home.html";}else{alert("Wrong Password");}}
+function openLetter(id){localStorage.setItem("id",id);window.location="letter.html";}
+let letters={
+"1":`Hey Nidhi..
+If you are opening this , that means you are feeling alone.
+Are baba, tmi eka nah.
+Ami always e tmr pashe asi.
+Maybe physically nah.
+But I always care about, think about you, and most importantly always love you.
+Tmk sob kichu eka handle kora lagbe nah toh. Ami asi tmr pashe.
+Tmr Bhalo din e. 
+Tmr Kharap din e.
+Tmr silent thakar din eo.
+Nidhi, tmi ekjon are kase onek matter koro.
+Sei ekjon ta ami.
+Yes beside your family members.
+Now, like a good girl, chokh ta bondho koro, imagine koro je amra Sohrawardhi teh, tmk dhore rakhsi, tmi tmr matha ta amr kadh e rakhso, batash onek, ashe pashe keo nai, duijon e chup chap,(NOW CLOSE YOUR EYES)
+I am here Nidhi.
+I am always here with you.`,
+"2":`Nidhi,
+why are u crying , jaan?
+Tmi kanna korle ektu o bhalo lage nah. Ami thakle tmr chokh muche ditam.
+You dont have to be strong all the times.
+You can cry. You can feel soft. Its okay.
+Tmk jeta kosto ditese amra eksathe face korbo.
+Tmk kichu explain kora lagbe nah. Nah tmk kichu pretend kora lagbe.
+Your emotions are safe with me. YOU are safe with me.
+Tmi amr jonno precious. Tmr chokh r pani toh aro precious.
+Let your tears fall. I will be still right here when they have stopped.
+`,
+"3":`Hey my princess,
+I know that heavy feeling. Ashole eitar jonno kono words o nai.
+I wish i could take half of your pain and carry it for you.
+Tmi eka suffer kora deserve koro nah.
+Sometimes life gets hard. But tmi er che onek strong.
+Lean on me. Even from far away.
+Tmk sob kichu eka hold kora lagbe nah. 
+I am here. 
+Call this no- 01534778545`,
+"4":`Aree Jaaan,
+You are missing me? I miss you more than you think. Words eo explain kora possible nah.
+I miss everything about you.
+Your voice.
+Your smell(specially hair).
+You hands.
+Damn, eita likhte gia o tmk miss kortesi.
+Dont worry Nidhi, Distance is temporary. Amder modhe jeta ase oita permanent.
+Eibhabe bhabo-
+"Joto time agache, 1sec koira o toto e amader kache ashar somoi ashtese."
+So remember, when you miss me:
+SOMEONE SOMEWHERE IS MISSING YOU JUST AS BADLY.
+Ar hain
+I love You babyy.`,
+"5":`Nidhi. 
+If you are reading this then we have argued. So please listen:
+You are not my enemy. You are mine.
+Amder majhe jhogra hoite pare, misunderstanding hoite pare.
+But ami tmk kosto dite chai nah. 
+I care about you more than you think.
+Dont ignore me.
+Dont go away.
+Lets talk.
+Lets Understand.
+Lets fix.
+Kotha bolo jaaan.
+Tmi e ekmatro friend amr. Keo nai kotha bolar. Tmi kotha nah bolle lonely lage. So please talk to me baby.
+I will choose you even on hard days.
+ALWAYS.`,
+"6":`Are baba,
+Ami tmk charbo kn?
+I will fight for you till last moments.
+Ami tmk emne emne choose kori nai. Ami choose korsi tmk biye korar jonno e. Tmr sathe bura howar jonno e. 
+Tmr sathe time pass korar kono chinta e amr nai.
+I see my future with you.
+People leave when they dont care. But i caree too much to walk away.
+You sudhu amr.
+Today. 
+Tomorrow.
+Always.
+`,
+"7":`Aree Nidhi?
+Tmr abr reassurance o dorkar?
+Ami jani eita tmi kokhonoi khulbe nah. But jodi khule thako then know it that:
+You are enough for me.
+Tmr proti ami koto obsessed eita tmi bhalo korei jano.
+Tmi jemon aso thik aso.
+Jodi paro kichu orange r pic dite paro i wont mind. 
+hahahaha.
+Ar pagol.
+Raat 2tai ami kar pic dekhi? Tmr e toh dekhi.
+Kake eto nongra nongra reel dei? Kake eto jalai?
+So eisob bhaibo nah.
+Jai hok.
+I love You the way you are.
+I am proud of you.
+I beleive in you.
+I am beside you.
+You are safe with me.`,
+"8":`Are Bacchaa..
+I love You more than u know or imagine.
+I love you when you’re happy.
+ I love you when you’re sad.
+ I love you when you’re jiddi baccha.
+I love you when u sleep on my shoulder on bus.
+I love you when you wear saree.
+I love you when you sleep on call.
+I love you when you care for me.
+I love you when you sing for me.
+I love you when u scold me for not taking care of myself.
+I love you when u send me pics. (ARO BESHI BHALOBASHBO JODI NAKED PIC DAO)
+I love you when u hold my hands specially my biceps.
+I love you when u let me touch you (AS TMI KAOKE DHORTE DAO NAH)
+Aro onek reasons ase..
+I didn’t fall for a perfect person.
+I fell for YOU.
+And I’m still falling
+Tmk ekta example dei. Je tmi amr jonno kih:
+Tmi jodi Mobile hou taile ami charger (I go inside u)
+heheheheheheh
+Jokes apart.
+FUCK YOU NIDHI.. (LOVE LANGUAGE)`,
+"9":`Aree Nidhi..
+Kharap kichu dekhso tar mane sopne..
+Dont worry my girl.
+It's just a dream.
+Nothings gonna happen.
+jodio tmi jano but
+Next time emn kichu kharap dekhle.
+"auzubillah minashaitan nirajeem"
+eita boilo ar jedin koira ghumaisila oitar opposite e ghumaiyo..
+Ar kharap lagle amake call dao. Share korle bhalo lagbe jodi beshi e kharap lage. 
+Asholei.
+Give it a try.
+Accha ekh Allah r naam nia ghumao.
+Go back to sleep knowing someone cares about you more than you realize.
+Sweet dreams.
+Hey Allah, Nidhi ke kharap sopne theke biroto rakho.
+`,
+"10":`Kih madam?
+Ghum ashtese nah?
+Isshh jodi ami thaktam tahole tmr mathai haat bulaiya ditam.
+Accha chokh bondho koro. Imagine koro tmi amr kadh e matha rakhso ar ami tmr mathai haat bulaiya ditase. Dekho bhalo lagbe.
+Ekbar try koira e dekho.
+Ar kichu nia chinta koiro nah.
+Sob thik hoiya jabe.
+You are braver than you realize.
+Ar Call deo eto e ghum nah ashle kotha boli.
+Good Night Nidhi.
+`,
+"11":`Tmi amr Voice sunte chao? Accha file ase audio sune nao.`,
+"12":`Hmm..
+Ik the future is tough and uncertain..
+Koto kichu hoite pare..
+Kichui jani nah..
+Ki hobe nah hobe..
+But Nidhi ekta jinish jani je future e jai hok tmi ar ami pasha pashi e thakbo. 
+We will figure those things out. Solve them. Step by step. One at a time.
+Sob kichu ajkei solve kora lagbe nah.
+So cool down.
+You already proved how much strong you are now.
+You acheived the things which some people can only dream off.
+So have faith in yourself and allah.
+Allah sob sohoz koira dibe.
+Insha Allah.
+Dont worry.
+You are not alone. I believe in you more than you beleive in yourself.`,
+"13":`Look who is happy and smiling.
+Thats right. My baby girl.
+Ajke tmi onek happy. Tmi happy mane amio happy.
+I wish i was there to see your smile.
+Ami Allah r kache dowa kori jeno Allah tmk emnei hashi khusi rakhe always.
+Ar jodi amr karon e tmi khusi hoiya thako..
+THEN MISSION ACCOMPLISHED...
+BIGGESHT ACHIEVEMENT OF THAT DAY..`,
+"14":`Jaaan,
+Look at you. You did it.
+Proud of you dear.
+Mah Girl afterall.
+Tmi onek efforts diso. Onek patient chila. Onek efforts diso.
+Finally those paid off.
+You deserve every bit of this success.
+Ami thakle tmk kole tule nitam.
+Bahire jaitam celebrate kortam.
+Aro onek kichu kortam.
+Jai hok. 
+I am so happy and proud of you Nidhii..
+I will be always your loudest supporter.`,
+"15":`Hey Nidhi..
+You opened this one that means something happened.
+Tmi kichu chaisi korte but paro nai.
+Dont worry Nidhi.
+Bhagge chilo nah.
+I know tmi onek try korso.
+Oitai onek.
+Amader haat e try kora chara kisui nai.
+Sobkichu r jonno e Hard Work r Little bit o luck lage.
+We can control the hard work but not our luck.
+So Allah ja kopal e likhse rakhse oitai.
+Amader dayitto hard work kora.
+Thats it.
+Dont be upset about it.
+Maybe something beautiful is waiting for u. Something more extraordinary.
+Ar no matter what, I am here.
+I love You.
+
+`,
+"16":`Something Special Occured Right?
+You wanna celebrate.
+But guess what? I am not here.
+Ami thakle tmk onek khon hug kortam. Kiss kortam. Ador kortam. Pore tmk nia bahire kothao ghurte jetam. 
+But nothing to worry. Kono ekdin eisob korbo.
+But now lets celebrate the moment.
+You deserve every moment.
+I am clapping for you.
+Smiling for you.
+Cheering for you.
+I am always with You.
+Way to go my little baby.`,
+"18":`You are opening this that means that u wanna feel special.
+Pagol.
+Tmi onek special amr jonno.
+In Every possible ways that i cant explain.
+You are my favorite person.
+ My safe place.
+ My comfort.
+Out of everyone in this world,
+I chose you.
+And I would choose you again.
+You are not replaceable.
+You are irreplaceable.
+Tmr toh eita bhaba o uchit nah je tmi special nah.
+Because special dekhe e tmi eirokom ekta small letter paccho.
+Tmi onek special dekhe e toh ami tmk eto bhalobashi.
+I REALLY LOVE YOU NIDHI.
+`,
+"19":`So You want to Imagine Our Future?
+Here it goes.
+Hoito kono ekta Friday.
+I am sleeping next to you. Hugging you from behind.
+Pore Nasta korlam.
+Bashar kaaj shesh holo.
+Jumma r namaz pore khawa dawa korlam. Pore bollam cholo Nidhi bahire jai kothao.
+Gari ta sokal e clean kore rakhsilam. Oita nia ber hoilam.
+Bkal e hoito emn kothao gelam jekhane khola mela akash.
+Piche amader gari.
+Samne kono lake.
+Tmr pashe ami boshe asi.
+Kotha bolte bolte bkal par hoye gelo.
+Pore kotha gelam bkal er nasta korte.
+Then gorom Cha khailam then basai ashlam.
+Then hoito kono movie dekhlam basai.
+Onek kichu e kora jai.
+Pore ghumaiya gelam.
+If u want we can do something. Bujhso e kiser bepare boltesi.
+hehehehehe.
+
+`,
+"20":`So its our wedding day.
+Its finally happening.
+Amra koto din dhore wait korsi ei din er jonno.
+So finally ei din ta ashlo.
+Tmi je amr bou hoba ta nah.
+Tmi amr best friend o.
+What can be more exciting than this.
+Tmi sob kichu ekjon r modhe paccho.
+A friend.
+A bestfriend.
+A boyfriend.
+A Male Bestie.
+A Husband.
+A life Partner.
+A soulmate.
+Ar hain thank you for choosing me. Ami jani nah amr modhe emn kih dekhso je bhalo lagse. Jeta e dekhso dekhte thako. 
+Amr onek tough time gese. Jekhane tmi chila. 
+Ekhono onek tough time ase but atleast tmi dure thakba nah.
+Officially amr bou e.
+Ami try korbo tmk khusi rakhte. Tmr jeno kono kosto nah hoi. Tmr chokh e jeno kono pani nah ashe.
+Finally amder purnota amra peye gelam Nidhi.
+I love you amr jaaan.`,
+"21":`Amr toh monehoi tmi eita porte o bhule jaba.
+Ami tmk mone korabo fs.
+So Nidhi finally we met.
+After a long long time.
+Onek kosto hoise ei koekta mash.
+Allah koto dhoirjo dhora lagse amr.
+Onek miss kori tmk.
+Jaak eisob shesh hoilo.
+Finally I could touch you.
+Feel your presence.
+Place my hands between your highs.
+uhmmm..
+Ajke onek bhalo ekta din.
+`,
+"22":`Aree Nidhi..
+Look at us..
+We moved In. Finally. Yes..
+Ekh saradin tmk dekhbo.
+Ekh tmk sokal theke raat porjonto dekhbo tmi kichu bolte parba nah.
+Eksathe ghumabo..
+Movie dekhte parbo..
+Tmi mojar mojar ranna kore khawaba..
+Ami biyer che ei din er beshi wait kortesi..
+Saradin por tmr face ta dekhbo
+amr din emnei bhalo hoiya jabe..
+Choto choto jinish nia tmk tease korbo jhogra korbo..
+Hotath kono kichu nia hashbo..
+Ami tmk bolbo towel ene dao tarpor towel er bahanai tmk bathroom e dhukabo..
+Tmr saree porte dekhbo
+Piche theke eshe hug korbo..
+Tmk khali gaye o dekhbo..
+Amr oranges gula..
+Tmr lunch tao khabo...
+Aiyeah..
+Jai hok...
+Onek moja hobee..
+Happy Move In Dear.`,
+"23":`Nidhi,
+You are no longer just my girlfriend.
+You are my forever.
+My partner.
+My Best Friend.
+My family.
+My home.
+Ami tmk sob somoi bhalobashbo.
+Tmk onek respect kori ekhono korbo.
+Tmr husband hoisi. But dont consider me as husband only.
+I can be whomever you want me to be.
+I am yours.
+Always.
+Love You..`,
+"24":`Nidhi,
+You are no longer just my girlfriend.
+You are my forever.
+My partner.
+My Best Friend.
+My family.
+My home.
+Ami tmk sob somoi bhalobashbo.
+Tmk onek respect kori ekhono korbo.
+Tmr husband hoisi. But dont consider me as husband only.
+I can be whomever you want me to be.
+I am yours.
+Always.
+Love You..`,
+"25":`Nidhi,
+I know I hurt you as u opened this letter.
+I am sorryy.
+Ami hurt korte chai nah.
+Tmk hurt korle nijei seta sashti pai.
+Eije tmi kotha bolo nah.
+Er theke beshi punishment amr jonno ki?
+You are my best friend.
+Saradin tmr sathe kotha boli.
+tmi nah thakle jeno pura din silence lage.
+So please forgive me.
+Tmi tmr jaigai thik.
+I take the responsibility.
+I am not perfect.
+I want to be better.
+FOR YOU.
+LOVE YOU NIDHI.
+`,
+"27":`Hey Nidhi, people grow but my love for you hasnt changed at all.
+Rather it is increasing day by day.
+Tmi joto e dure thako ami tmk nia e chinta kori, tmk nia e bhabi.
+Jodi dekho ami kotha boltesi nah or off vibes dicchi taile bujhe nio ami kichu nia besto or chintiteo.
+But tmi ekta msg dile ami sob bhule jabo.
+Tmi jodi bolo Kabid tmi thik aso?(TMI KORE BOLLE AMI BESHI KHUSII HOII NGL)
+Mane jiggesh korle
+Ami ignore korbo nah kokhono.
+Dont assume that i stopped loving you.
+I am still here Nidhi.`,
+"28":`Jaaan
+I am Sorry.
+I am so sorry.
+For hurting you.
+For the words that i shouldnt have said.
+For treating you badly.
+For yelling at you.
+For misunderstanding you.
+For breaking your faith.
+
+Ami jani ami tmk dissapoint korsi.
+But please amake maaf kore dao.
+Ami ek mistake duibar kori nah.
+So please Nidhi forgive me.
+I am trying to become better.
+
+Talk to me Nidhi please.`,
+"29":`Nidhi,
+I dont know what to say actually.
+I am sorry je tmr expectation meet korte pari nai.
+Ami tmk let down korsi.
+Ami eirokom kokhonoi chai nai.
+Tmi amr upor dissapointed hou is the last thing i wanted.
+Tmi amr biggest supporter so i dont want you to be dissapointed.
+Please mone raikho je
+Ami try kori
+Amio sikhtesi
+So please dont give up on me.
+Ami amader upor give up kori nai.
+Love you Nidhi always.
+`,
+"30":`Hey Baby,
+Its ur birthday.
+The day when you came to this world.
+The day a cute jiddi baccha was born.
+Thank you Nidhi for existing.
+Tmi eita porteso tar mane ekhono amra long distance e asi.
+Kache thakle onek kichu kortam.
+Thak oita tmr bday teh korle bolbo.
+But i got plans.
+Allah tmk always hashi khusi rakhuk r sustho rakhuk.
+You are my precious and my most favourite gift.`,
+"31":`Bacchaa,
+Amader anniversay.
+Another year of our togetherness.
+Lesgooo.
+Proud of us.
+Koto e jhogra na hoise.
+Koto nah misunderstanding.
+But in the end we are together.
+Thank you for showing patience and faith in me.
+Thank you for staying with me.
+I love you Nidhi.
+Today and Forever.`,
+"32":`I know you said not to say anything.
+But eita toh bola e jai Nidhi je.
+Bigoto bochor er sob bhul maaf kore dio amr.
+Ar notun bochor er bhul gula o maaf kore dio.
+Last year er prottek din jemon ami tmr pashe chilam.
+Even on good days and bad days.
+Eibhabe next year er bhalo r kharap din e tmr pashe thakte chai.
+Every day, every year my love for u is increasing than the previous one.
+Love you till the end.`,
+"33":`Amr Baccha,
+ Jodi din ta onek kharap hoi tahole,
+amr kadh e helan dao.
+kanna koro dorkar porle.
+ chup chap thako amr pashe.
+You don’t have to be okay.
+I’m here.
+Holding your hand through words.
+We will survive this day.
+Together.
+Call me Nidhi.
+`,
+"34":`Its your best day.
+Yeeee.
+lesgoo baby girl.
+Emnei jeno hashi khusi thako.
+Prottek din e jeno tmr jonno best day hou.
+Enjoy koro.
+Celebrate koro.
+Ami tmr pashe ashi from far away.
+Your happiness is the most important thing to me.
+Proud of you Nidhi.`,
+"35":`Eita tmi open korba ami bissash e korte pari nah.
+Khulle o for fun.
+Asche.
+Asholei feel korle ekta e kotha bolbo..
+Lagao video call.
+Khulo tmr dress.
+Tmr dudh gula dekhbo.
+Asho duijon eksathe kori video call e.`
+};
+window.onload=function(){
+let id=localStorage.getItem("id");
+if(id){document.getElementById("title").innerText="Open When";
+document.getElementById("text").innerText=letters[id];}
+}
